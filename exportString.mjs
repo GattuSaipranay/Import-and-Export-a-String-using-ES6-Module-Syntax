1e83269d86dffa551d25312c8fb0e0e9e0ff0985
GattuSaipranay <saipranaysrinivas@gmail.com>
2024-03-02 11:32:55 +0000

    Import and Export a String using ES6 Module Syntax

---
+++ exportString.mjs
@@ -0,0 +1,5 @@
+// Define the string to export
+const myString = "Be Happy and Safe";
+
+// Export the string using default export syntax
+export default myString;
---
+++ importString.mjs
@@ -0,0 +1,5 @@
+// Import the default export
+import myString from './exportString.mjs';
+
+// Test the imported string
+console.log("Imported string:", myString);
